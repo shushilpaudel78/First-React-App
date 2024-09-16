661cd37135afdab1156bac1b9e8959b5a5e1ef2d
Shushil <84438191+shushilpaudel78@users.noreply.github.com>
2024-09-16 13:28:42 +0545

    initial commit 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Nav from './components/Nav'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,12 +10,7 @@ function App() {
   return (
     <>
 
-    <h1>Hello from app  page</h1>
-
-    
-    
-
-    
+    <Nav />
     
       
     </>
---
+++ src/components/Nav.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <>
+
+    <div className="w-full h-auto m-1">
+      <div className="max-w-[1200px] h-auto m-auto" >
+
+
+      </div>
+
+
+
+    </div>
+
+
+
+
+
+    </>
+   
+  )
+}
+
+export default Nav
