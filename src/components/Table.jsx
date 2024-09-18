2972ca4bcda298ec708f7384417daa24f31e3aee
Shushil <84438191+shushilpaudel78@users.noreply.github.com>
2024-09-18 12:05:43 +0545

    push type

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import Button from './Button'
+import Bitcoin from '../assets/img/bitcoin.png'
+import Group from '../assets/img/Group.png'
+
+
 import { height, width } from '@fortawesome/free-solid-svg-icons/fa0'
 
 export default function Table() {
 
     const costumeStyle = {
-        // backgroundColor:'red',
-        height:'50px',
+        height: '50px',
+        // background: 'red',
+        border: '2px solid black'
 
-        
     }
     return (
         <div>
-            <table className='w-full text-center bg-white py-4 font-poppins text-sm table_custo' >
+            <table className='w-full text-center bg-white py-4 font-poppins text-sm  min-w-full divide-y divide-gray-200' >
                 <tr className='' style={costumeStyle}>
                     <th >Date </th>
                     <th>Transition ID </th>
@@ -28,7 +32,13 @@ export default function Table() {
                 <tr style={costumeStyle}>
                     <td>14/01/2019</td>
                     <td>12345678</td>
-                    <td>Type</td>
+                    <td>
+
+                        <div className="w-[20px] h-[20px] flex">
+                            <img src={Bitcoin} alt="bitcoin img" className='w-full h-full' />
+                        </div>
+
+                    </td>
                     <td>Amazon</td>
                     <td>$355</td>
                     <td>#18000</td>
@@ -37,6 +47,23 @@ export default function Table() {
 
                 </tr>
 
+
+                <tr style={costumeStyle}>
+                    <td>14/01/2019</td>
+                    <td>12345678</td>
+                    <td>
+                        <div className="w-[20px] h-[20px] flex">
+                            <img src={Group} alt="group img" className='w-full h-full' />
+                        </div>
+
+                    </td>
+                    <td>Amazon</td>
+                    <td>$355</td>
+                    <td>#18000</td>
+                    <td>Abc</td>
+                    <td><Button name="shushil" /></td>
+
+                </tr>
 
                 <tr style={costumeStyle}>
                     <td>14/01/2019</td>
@@ -62,18 +89,6 @@ export default function Table() {
 
                 </tr>
 
-                <tr style={costumeStyle}>
-                    <td>14/01/2019</td>
-                    <td>12345678</td>
-                    <td>Type</td>
-                    <td>Amazon</td>
-                    <td>$355</td>
-                    <td>#18000</td>
-                    <td>Abc</td>
-                    <td><Button name="shushil" /></td>
-
-                </tr>
-
 
                 <tr style={costumeStyle}>
                     <td>14/01/2019</td>
@@ -83,7 +98,7 @@ export default function Table() {
                     <td>$355</td>
                     <td>#18000</td>
                     <td>Abc</td>
-                    <td><Button name="shushil" /></td>
+                    <td><Button name="Delete" /></td>
 
                 </tr>
                 <tr style={costumeStyle}>
